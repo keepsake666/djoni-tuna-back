4ef12110a45227e883f4f13ccbaecb4a09bd01fd
Sergey Veselov <veselovsa@list.ru>
2023-12-08 15:11:22 +0900

    deploy vercel

--- config/plugins.js
+++ config/plugins.js
@@ -3,10 +3,9 @@ module.exports = ({ env }) => ({
     enabled: true,
     config: {
       deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+        "https://api.vercel.com/v1/integrations/deploy/prj_pqAZvVkCihTNwGDWAD5WGrDZmuxD/1sFYAfYJHg",
       apiToken: "BnJlh0NjZT4LCdRNo7Q79Pu5",
-      appFilter: "keepsake666",
-      teamFilter: "your-team-id-on-vercel",
+      appFilter: "djoni-tuna-back",
       roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
   },
